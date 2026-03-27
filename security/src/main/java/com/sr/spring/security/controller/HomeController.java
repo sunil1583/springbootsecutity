f6794ef97b026e7f194df7752e01865ff6668201
sunil1583 <sunil1583@gmail.com>
2026-03-27 14:56:41 +0100

    massage changed 2

--- security/src/main/java/com/sr/spring/security/controller/HomeController.java
+++ security/src/main/java/com/sr/spring/security/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
     //http://localhost:8080/public
     @GetMapping("/public")
     public String Home() {
-        return "Hello Home! I am here. This is working...";
+        return "Hello Home! I am here. This is working.... Thank you!";
     }
 
     //http://localhost:8080/secured
