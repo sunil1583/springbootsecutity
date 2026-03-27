eacd8f630f659454d1abbeb41a7ebf303f091528
sunil1583 <sunil1583@gmail.com>
2026-03-27 21:17:00 +0100

    message corrected

--- security/src/main/java/com/sr/spring/security/controller/HomeController.java
+++ security/src/main/java/com/sr/spring/security/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
     //http://localhost:8080/public
     @GetMapping("/public")
     public String Home() {
-        return "Hello Home! I am here. This is working.... Thank you!";
+        return "Hello Home!";
     }
 
     //http://localhost:8080/secured
